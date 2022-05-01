284a59d64c3f6ed19a6b35ced1b63077d142ce1d
Elizabeth Sobiya <98687741+ElizabethSobiya@users.noreply.github.com>
2022-05-01 23:25:52 +0530

    added

---
+++ App.js
@@ -0,0 +1,46 @@
+
+// import React, { Component } from "react";
+// import AdminPanel from './AdminPanel/AdminPanel'
+import axios from 'axios';
+import GridDesign from './GridDesign/GridDesign.js';
+// import './GridDesign.css'
+import { Component } from 'react';
+
+
+
+export class App extends Component{
+  render(){
+    return(
+      <div>
+        <GridDesign/>
+      </div>
+    )
+  }
+}
+
+// function App(){
+//   return(
+//     <div>
+//        <GridDesign/>
+//     </div>
+//   )
+// }
+
+
+// export class App extends Component{
+
+  
+//   render (){
+//   return (
+//     <div>
+//     
+//       {/* <AdminPanel/> */}
+//     </div>
+//   )
+// }
+// }
+
+
+
+
+export default App;
